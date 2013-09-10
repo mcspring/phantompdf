3205f7e35b528fd4691b95af526aed0206d84848
Spring MC <Heresy.Mc@gmail.com>
2013-09-10 10:49:52 +0800

    new :be_pdf_file and :be_pdf_string matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,24 @@
 require 'phantomjs'
 require 'phantompdf'
+require 'byebug'
 
 RSpec.configure do |config|
   # some staff goes here
+end
+
+RSpec::Matchers.define :be_pdf_file do
+  match do |actual|
+    case actual
+    when File
+      actual.read[0...4] == '%PDF'
+    when String
+      File.exist?(actual) && File.open(actual).read[0...4] == '%PDF'
+    end
+  end
+end
+
+RSpec::Matchers.define :be_pdf_string do
+  match do |actual|
+    actual[0...4] == '%PDF'
+  end
 end
