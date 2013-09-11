ad79d26d447af0fa9096cdd3a8aa96b6f8704b82
Spring MC <Heresy.Mc@gmail.com>
2013-09-11 11:33:27 +0800

    refactor PhantomPDF.configure

--- lib/phantompdf.rb
+++ lib/phantompdf.rb
@@ -7,4 +7,30 @@ require 'phantompdf/generator'
 require 'phantompdf/middleware'
 
 module PhantomPDF
+  # Configure PhantomPDF someplace sensible,
+  # like config/initializers/phantompdf.rb
+  #
+  # @example
+  #   PhantomPDF.configure do |config|
+  #     config.format            = 'A4',
+  #     config.header            = nil,
+  #     config.footer            = nil,
+  #     config.margin            = '1cm',
+  #     config.orientation       = 'portrait',
+  #     config.zoom              = 1,
+  #     config.cookies           = {},
+  #     config.timeout           = 90000,
+  #     config.rendering_timeout = 1000
+  #   end
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Config.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
 end
